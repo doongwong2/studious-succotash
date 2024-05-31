27f2cf359753590fac0d05c62b9b5bb96b78d921
doongwong2 <145019137+doongwong2@users.noreply.github.com>
2024-05-31 10:49:14 +0800

    Create 260.go

---
+++ LeetCode/260.go
@@ -0,0 +1,25 @@
+func getBit(x int, k int) int {
+    return (x >> k) & 1
+}
+
+func singleNumber(nums []int) []int {
+    xorXY := 0
+    for _, num := range nums {
+        xorXY ^= num
+    }
+
+    pos := 0
+    for getBit(xorXY,pos) == 0 {
+        pos++
+    }
+
+    x:= 0 
+    for _, num := range nums {
+        if getBit(num, pos) != 0 {
+            x ^= num
+        }
+    }
+
+    y := xorXY ^ x
+    return []int{x,y}
+}
